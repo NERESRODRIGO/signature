760fe2267a8028d322e7e159b596ca98e7838dab
RODRIGO NERES <neres.rodrigo@hotmail.com>
2020-09-12 22:22:13 -0300

    Iniciando o sistema

---
+++ assinatura.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nome=$_POST['nome'];
+    $nome=$_POST['cargo'];
+    $nome=$_POST['telefone'];
+
+    if(isset($_POST['escolha']==imagem)){
+
+    }else{
+        
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form method="POST" action="assinatura.php"> 
+            <input type="text" placeholder="Nome"></input><br>
+            <input type="text" placeholder="Cargo"></input><br>
+            <input type="text" placeholder="Telefone/Ramal"></input><br>
+            <input type="radio" id="imagem" name="escolha" value="imagem">Imagem</input>
+            <input type="radio" id="html" name="escolha" value="html">Html</input><br>
+        
+            <button type="submit">Gerar Assinatura</button>
+        </form>
+    </body>
+</html>
