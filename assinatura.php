0ebe562a303ebd7642cdad00b6accb92d1e9672a
Rodrigo Neres <neres.rodrigo@hotmail.com>
2020-09-15 07:41:28 -0300

    Incluindo modelo de assinatura em html

--- assinatura.php
+++ assinatura.php
@@ -1,14 +1,45 @@
 <?php
 
     $nome=$_POST['nome'];
-    $nome=$_POST['cargo'];
-    $nome=$_POST['telefone'];
+    $cargo=$_POST['cargo'];
+    $telefone=$_POST['telefone'];
 
-    if(isset($_POST['escolha']==imagem)){
+    /*if(isset($_POST['escolha']==imagem)){
 
     }else{
         
-    }
+    }*/
+
+// Gerando imagem
+
+
+// Gerando html
+
+       <style>
+            table tr td {
+                font-size: 20px;
+                padding-left: 15;
+            }
+        </style>
+        <table>
+            <tr>
+                <td rowspan="5">
+                    <img src="http://www.registro.sp.gov.br/images/logo-registro-n.png" width="160">
+                </td>
+            </tr>
+            <tr>
+                <td><?php echo $nome ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $cargo ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $telefone ?></td>
+            </tr>
+            <tr>
+                <td><a href="http://www.registro.sp.gov.br/Default.asp">Prefeitura Municipal de Registro</a></td>
+            </tr>
+        </table>
     <ul>
     <li><button>Baixar Imagem</button></li>
     <li><button>Baixar Html</button></li>
